910df17d5932630f1f0d1591c7a1f9f87f700bb6
Ger Bruinsma <ger_bruinsma@hotmail.com>
2018-01-03 15:51:20 +0100

    url fix

--- event/main_listener.php
+++ event/main_listener.php
@@ -101,7 +101,7 @@ class main_listener implements EventSubscriberInterface
     }
     
     /**
-     * Send reuired vars to template
+     * Send required vars to template
      * @param object $event The event object
      * @return null
      * @access public
@@ -112,7 +112,7 @@ class main_listener implements EventSubscriberInterface
         $this->lang->add_lang('common', 'ger/livetopicupdate');
         $this->template->assign_vars(array(
             'U_LIVEUPDATE'      => $this->helper->route('ger_livetopicupdate_controller', ['tid' => $event['topic_id'], 'old' => $event['total_posts']]),
-            'U_REFRESH'         => append_sid($this->phpbb_root_path . 'viewtopic.' . $this->php_ext . '?f=' . $event['forum_id'] . '&t=' . $event['topic_id'] . '&view=unread#unread'),
+            'U_REFRESH'         => append_sid($this->phpbb_root_path . 'viewtopic.' . $this->php_ext . '?f=' . $event['forum_id'] . '&p=' . $lastpost ),
             'S_LTU_INTERVAL'	=> (int) $this->config['ger_livetopicupdate_interval'] * 1000,
         ));
     }
